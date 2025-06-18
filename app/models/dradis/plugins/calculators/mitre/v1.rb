badf888505b1287753bec9bd6b47db0bc1059c0e
Matt Budz <matt.budz@protonmail.com>
2025-06-18 17:32:04 +0200

    update fields for clarity and add IDs

--- app/models/dradis/plugins/calculators/mitre/v1.rb
+++ app/models/dradis/plugins/calculators/mitre/v1.rb
@@ -1,9 +1,24 @@
 module Dradis::Plugins::Calculators::MITRE
   class V1
     FIELD_NAMES = %i[
-      tactics
-      technique
-      sub-technique
+      Enterprise.Tactic
+      Enterprise.Tactic.ID
+      Enterprise.Technique
+      Enterprise.Technique.ID
+      Enterprise.Sub-technique
+      Enterprise.Sub-technique.ID
+      Mobile.Tactic
+      Mobile.Tactic.ID
+      Mobile.Technique
+      Mobile.Technique.ID
+      Mobile.Sub-technique
+      Mobile.Sub-technique.ID
+      ICS.Tactic
+      ICS.Tactic.ID
+      ICS.Technique
+      ICS.Technique.ID
+      ICS.Sub-technique
+      ICS.Sub-technique.ID
     ].freeze
 
     FIELDS = FIELD_NAMES.map { |name| "MITRE.#{name}".freeze }.freeze
